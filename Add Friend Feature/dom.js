4ef4628c525808d13e95858510b99e0d7710bac3
Ayush-Yadav <ayush421301@gmails.com>
2024-03-25 13:24:11 +0530

    Added Confirmation Prompt on Removal of Friend

--- Add Friend Feature/dom.js
+++ Add Friend Feature/dom.js
@@ -1,21 +1,32 @@
 var a = document.querySelector("h3");
+
 var b = document.querySelector("#add");
+
 var flag = 0;
 
-add.addEventListener("click", function() {
-    if (flag == 0) {
+add.addEventListener("click", function()
+ {
+    if (flag == 0) 
+    {
         a.innerHTML = "Friends";
         a.style.color = "green";
-        b.style.backgroundColor = "#dadada"; 
+        b.style.backgroundColor = "#dadada";
         b.style.color = "black";
-        b.innerHTML = "Remove"; 
+        b.innerHTML = "Remove";
         flag = 1;
-    } else {
-        a.innerHTML = "Stranger";
-        a.style.color = "red";
-        b.style.backgroundColor = ""; 
-        b.style.color = ""; 
-        b.innerHTML = "Add Friend"; 
-        flag = 0;
+    } 
+    else
+     {
+        var confirmRemove = window.confirm("Are you sure you want to remove this friend?");
+
+        if (confirmRemove)
+         {
+            a.innerHTML = "Stranger";
+            a.style.color = "red";
+            b.style.backgroundColor = "";
+            b.style.color = "";
+            b.innerHTML = "Add Friend";
+            flag = 0;
+        }
     }
 });
