a680f3ea323c3e4a25276d96b3aef4d8bef60d32
Ayush-Yadav <ayush421301@gmails.com>
2024-03-25 13:13:21 +0530

    Added Remove Option When "Clicked"

--- Add Friend Feature/dom.js
+++ Add Friend Feature/dom.js
@@ -1,18 +1,21 @@
 var a = document.querySelector("h3");
-
 var b = document.querySelector("#add");
-
 var flag = 0;
 
 add.addEventListener("click", function() {
     if (flag == 0) {
         a.innerHTML = "Friends";
         a.style.color = "green";
+        b.style.backgroundColor = "#dadada"; 
+        b.style.color = "black";
+        b.innerHTML = "Remove"; 
         flag = 1;
     } else {
         a.innerHTML = "Stranger";
         a.style.color = "red";
+        b.style.backgroundColor = ""; 
+        b.style.color = ""; 
+        b.innerHTML = "Add Friend"; 
         flag = 0;
     }
 });
-
